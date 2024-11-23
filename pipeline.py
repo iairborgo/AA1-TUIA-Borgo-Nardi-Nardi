2031ae0a5c754629ad3a3cd2ab93823dcf04cde1
Iair Borgo Elgart <105130558+iairborgo@users.noreply.github.com>
2024-11-23 18:45:11 -0300

    Update pipeline.py
    terminado modelo final

--- pipeline.py
+++ pipeline.py
@@ -5,6 +5,7 @@ from imblearn.over_sampling import RandomOverSampler
 from sklearn.preprocessing import PowerTransformer, MinMaxScaler
 from sklearn.impute import KNNImputer, SimpleImputer
 from pipeline_tools import *
+from skorch import NeuralNetBinaryClassifier
 
 df = pd.read_csv('weatherAUS.csv')
 df[df['Location'] == 'MountGini']['Location'] = 'MountGinini'
@@ -26,13 +27,17 @@ ros = RandomOverSampler()
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify = y)
 X_train, y_train = ros.fit_resample(X_train, y_train)
 
-power_cols = ['MinTemp', 'MaxTemp', 'Rainfall', 'Evaporation', 'WindGustSpeed','Humidity9am', 'Humidity3pm', 'Pressure3pm']
-minmax_cols = ['Cloud9am', 'Cloud3pm']
-categorical_cols = ['RainToday']
-all_columns =  power_cols + minmax_cols + ['RainToday','sin_week', 'cos_week']
-
-
 drop = ['Location', 'WindGustDir', 'Temp3pm', 'Temp9am', 'Sunshine', 'WindSpeed9am', 'WindSpeed3pm', 'WindDir9am', 'WindDir3pm', 'Pressure9am']
+
+model = NeuralNetBinaryClassifier(
+    RainClassifier,
+    criterion=torch.nn.BCEWithLogitsLoss,
+    optimizer=torch.optim.Adam,
+    lr=0.0075,
+    max_epochs=150,
+    batch_size=128,
+    verbose=False
+)
 
 transform_pipeline = Pipeline(steps=[
     ('binarize_rain', BinaryEncoder('RainToday')),
@@ -43,5 +48,6 @@ transform_pipeline = Pipeline(steps=[
     ('impute_scale_cloud', FunctionTransformer(impute_scale_cloud)),
     ('drop', FunctionTransformer(drop_columns, kw_args={'columns': drop})),
     ('knn_imputer',  KNNImputer(n_neighbors=155)),
-    #modelo
+    ('float32', FunctionTransformer(func=lambda X: torch.tensor(X, dtype=torch.float32),validate=False)),
+    ('model' model.initialize())
 ])
